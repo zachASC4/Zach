7f6414efd7896c5895fe945065ea1eaec01c3de2
zachASC4 <lovezoo2000@gmail.com>
2017-07-19 13:04:20 -0400

    update

---
+++ zach-battleship/sketch.js
@@ -0,0 +1,65 @@
+function setup(){
+    createCanvas(800,800);
+    background(225);
+}
+
+function draw(){
+ for(i = 0; i < 8; i++){
+
+ }
+}
+
+	// var height = 8;
+	// var width = 8;
+	// var totalBoats = 4;
+    // var totalGuesses = 3;
+	// var water = [height][width];
+
+	// for (var i = 0; i < water.length; i++) {
+	//     for (var j = 0; j < water.length; j++) {
+	// 	water[i][j] = '~';
+	//     }
+	// }
+
+	// var shipCount = totalBoats;
+	// while (shipCount > 0) {
+	//     var ship;
+	//     var boat;
+	//     ship = Math.round(Math.Random()*height);
+	//     boat =  Math.round(Math.Random()*width);
+	//     water[ship][boat] = 'X';
+	//     shipCount--;
+	// }
+	
+
+	//     var guesses = totalGuesses;
+	//     while (guesses > 0) {
+	// 	console.log("What row would you like to guess?");
+	// 	var row = mouseClicked();
+	// 	console.log("What column would you like to guess?");
+	// 	var column =  mouseClicked();
+	// 	if (row > height || column > width) {
+	// 	    console.log("Not a valid answer");
+		    
+	// 	}
+
+	// 	if (water[row][column] == 'X') {
+	// 	    water[row][column] = 'H';
+	// 	} else {
+	// 	    water[row][column] = 'M';
+	// 	}
+	// 	if (water[row][column] == 'H' || water[row][column] == 'M'){
+	//     console.log("You already guessed that.");
+	// 	}
+		
+	// 	for (var i = 0; i < water.length; i++) {
+	// 	    for (var j = 0; j < water.length; j++) {
+	// 		if (water[i][j] == 'X') {
+	// 		    console.log('~');
+	// 		} else {
+	// 		 console.log(water[i][j]);
+    //         }
+    //     } 
+    // }
+    //     }
+    
