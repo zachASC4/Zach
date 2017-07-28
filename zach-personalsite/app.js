f3721a82376f3ea3eb582a9f80144fea7d3ab974
zachASC4 <lovezoo2000@gmail.com>
2017-07-28 15:45:35 -0400

    update

---
+++ zach-personalsite/app.js
@@ -0,0 +1,43 @@
+// <!--
+// <div class="container">
+//   <h2></h2>  
+//   <div id="myCarousel" class="col-sm-8 carousel slide" data-ride="carousel">
+//                 <!-- Indicators -->
+//                 <ol class="carousel-indicators">
+//                     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+//                     <li data-target="#myCarousel" data-slide-to="1"></li>
+//                     <li data-target="#myCarousel" data-slide-to="2"></li>
+//                      <li data-target="#myCarousel" data-slide-to="3"></li>
+//                 </ol>
+
+//                <!-- Wrapper for slides -->
+//     <div class="carousel-inner">
+//       <div class="item active">
+//         <img src="https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/16265520_396533670694189_4863530832084367192_n.jpg?oh=7deceec4bc0a1f8903848a0bda045fd4&oe=5A0A3B6A">
+//       </div>
+
+//      <div class="item">
+//         <img src="https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/12733428_217017548645803_8162551722800953993_n.jpg?oh=d6d0d54e03512a1952c745c8cd8ae162&oe=5A38AEF8">
+//       </div>
+    
+//      <div class="item">
+//         <img src="https://media.contentapi.ea.com/content/www-easports/en_US/madden-nfl/news/2016/madden-17-additional-gameplay-improvements-zone-coverage/_jcr_content/par/image_0.img.jpg">
+//       </div>
+
+//     <!--<div class="item">
+//         <img src="" style="object-fit:contain" class="img-responsive center-block">
+        
+//         </div>-->
+//     </div>
+//     <!-- Left and right controls -->
+//     <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+//       <span class="glyphicon glyphicon-chevron-left"></span>
+//       <span class="sr-only">Previous</span>
+//     </a>
+//     <a class="right carousel-control" href="#myCarousel" data-slide="next">
+//       <span class="glyphicon glyphicon-chevron-right"></span>
+//       <span class="sr-only">Next</span>
+//     </a>
+//   </div>
+// </div>-->
+// */
