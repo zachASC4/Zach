a2c0ba7376c670d68ce02412766dcf826c5bfdfe
zachASC4 <lovezoo2000@gmail.com>
2017-08-15 17:16:56 -0400

    update

---
+++ demo/getMessages.js
@@ -0,0 +1,28 @@
+function appendMessageRow(message) {
+  $('.table-inbox tbody').append(
+    '<tr>\
+      <td>'+getHeader(message.payload.headers, 'From')+'</td>\
+      <td>'+getHeader(message.payload.headers, 'Subject')+'</td>\
+      <td>'+getHeader(message.payload.headers, 'Date')+'</td>\
+    </tr>'
+  );
+}
+
+function displayInbox() {
+  var request = gapi.client.gmail.users.messages.list({
+    'userId': 'me',
+    'labelIds': 'INBOX',
+    'maxResults': 10
+  });
+
+  request.execute(function(response) {
+    $.each(response.messages, function() {
+      var messageRequest = gapi.client.gmail.users.messages.get({
+        'userId': 'me',
+        'id': this.id
+      });
+
+      messageRequest.execute(appendMessageRow);
+    });
+  });
+}
